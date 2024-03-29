f1a1dc71eb1402f28110c0dd13b169cf9d036b3d
yovika1 <yovikaranga@gmail.com>
2024-03-29 23:34:06 +0530

    some update

--- admin-pannel/src/page/ProductList.js
+++ admin-pannel/src/page/ProductList.js
@@ -1,11 +1,112 @@
-import React from 'react'
+// ProductList.js
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const getProduct = async () => {
+    setLoading(true);
+    try {
+      const getRes = await axios.get("http://localhost:8000/getData");
+      console.log(getRes);
+      setProducts(getRes?.data?.products);
+      setError(null);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  useEffect(() => {
+    console.log(products.products);
+  }, [products]);
+
   return (
-    <div className=' ml-64'>
-         <div className=' h-14 mt-32 text-blue-700 font-semibold'>
-         ProductList
-       </div>
+    <div className="ml-64">
+      <div className="h-14 mt-2 text-blue-700 font-semibold">
+        <h1>Product List</h1>
+      </div>
+
+      {error && <div className="text-red-600">{error}</div>}
+
+      {loading && <div>Loading...</div>}
+
+      <div className="overflow-x-auto shadow-md rounded-lg">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-4 py-3">
+                <span className="sr-only">Image</span>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Product
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Qty
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Price
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Dis
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Description
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.isArray(products) &&
+              products.map((item) => (
+                <tr
+                  key={item.id}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                >
+                  <td className="p-4">
+                    <img
+                      src={"http://localhost:8000/" + item.url}
+                      className="w-16 md:w-32 max-w-full max-h-full"
+                      alt={item.productTitle}
+                    />
+                  </td>
+                  <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                    {item.productTitle}
+                  </td>
+                  <td className="px-6 py-4">{item.quantity}</td>
+                  <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                    {item.price}
+                  </td>
+                  <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                    {item.discount}
+                  </td>
+                  <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                    {item.productBrief}
+                  </td>
+                  <td className="px-6 py-4">
+                    <Link
+                      to="#"
+                      className="font-medium text-red-600 dark:text-red-500 hover:underline"
+                    >
+                      Remove
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
